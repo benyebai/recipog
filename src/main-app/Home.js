a6695512d09f75f8023f14f654c46905e4ed6ae0
benyebai <benyebai@gmail.com>
2022-06-15 14:52:47 -0700

    i gamed

--- src/main-app/Home.js
+++ src/main-app/Home.js
@@ -10,64 +10,88 @@ export class Home extends React.Component {
         super(props);
         this.state = {
             search: "",
-            data: null,
-            bruh: "penis"
+            data: null
         }
         
     }
 
     async componentDidMount() {
-        const options = {
-            method: 'GET',
-            url: 'https://tasty.p.rapidapi.com/feeds/list',
-            params: {size: '5', timezone: '+0100', vegetarian: 'false', from: '0'},
-            headers: {
-                'X-RapidAPI-Key': '8b3b76d149msh8e93702247b0dcfp19dd88jsn9f526fa40eda',
-                'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-            }
-        };
         
-        await axios.request(options)
+        let date_ob = new Date();
+        let date = ("0" + date_ob.getDate()).slice(-2);
+        let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+        let year = date_ob.getFullYear();
+
         
-        .then((response) => {
-
-            for (let i = 0; i < response.data.results.length; i++) {
-                if (response.data.results[i].name === "Trending") {
-                    this.setState({data: response.data.results[i].items})
-                    
-                    console.log(response.data.results[i].items)
-
-                    let dataa = []
-                    for (let j = 0; j < 5; j++) {
-                        dataa.push(response.data.results[i].items[j])
+        await axios.get("http://localhost:3001/api/gettrending/" + year + "-" + month + "-" + date)
+        .then((res) => {
+            console.log(res.data.data)
+            if (res.data.data == null) {
+                
+                const options = {
+                    method: 'GET',
+                    url: 'https://tasty.p.rapidapi.com/feeds/list',
+                    params: {size: '5', timezone: '+0100', vegetarian: 'false', from: '0'},
+                    headers: {
+                        'X-RapidAPI-Key': '8b3b76d149msh8e93702247b0dcfp19dd88jsn9f526fa40eda',
+                        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
                     }
+                };
+                
+                axios.request(options)
+                
+                .then((response) => {
 
-                    axios.post("http://localhost:3001/api/trending", {data: dataa})
+                    for (let i = 0; i < response.data.results.length; i++) {
+                        if (response.data.results[i].name === "Trending") {
+                            
+                            let dataa = []
+                            for (let j = 0; j < 6; j++) {
+                                dataa.push(response.data.results[i].items[j])
+                            }
 
-                    .then((res) => {
-                        console.log(res)
-                    })
+                            this.setState({data: dataa})
 
-                    .catch((err) => {
-                        console.log("error");
-                    });
-                }
+                            axios.post("http://localhost:3001/api/storetrending", {data: dataa})
+
+                            .then((res) => {
+                                console.log(res)
+                            })
+
+                            .catch((err) => {
+                                console.log("error");
+                            });
+                        }
+                    }
+                    
+                    
+                })
+                .catch(function (error) {
+
+                    console.error(error);
+                });
+
+                
+            } else {
+                this.setState({data: res.data.data})
             }
-            
-            
         })
-        .catch(function (error) {
 
-            console.error(error);
-        });
+        
     }
 
     render() {
         
         if (this.state.data) {  
+
+            let displayedTrending = []
+            for (let i = 0; i < this.state.data.length; i++) {
+                displayedTrending.push(<DisplayBasic data={this.state.data[i]} key={i} />)
+            }
+
             return(
                 <div className="main">
-                    <DisplayBasic data={this.state.data[0]} />
+                    {displayedTrending}
                 </div>
             );
         }
